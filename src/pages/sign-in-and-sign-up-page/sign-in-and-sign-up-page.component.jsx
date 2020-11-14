0365699d8dea98aa88fb0e827b6ae0db50286da2
Bob Rusch <rvrusch@gmail.com>
2020-11-14 14:32:22 -0500

    added sign in page

---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up-page.styles.scss';
+
+const SingInAndSignUpPage = () => (
+  <div>
+    <div className='sign-in-and-sign-up'><SignIn /></div>
+  </div>
+)
+
+export default SingInAndSignUpPage;
