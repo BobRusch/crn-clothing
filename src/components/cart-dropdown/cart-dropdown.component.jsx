b7d6573727704bd421a0bb4fbee627f9d83de9c5
Bob Rusch <rvrusch@gmail.com>
2020-11-17 13:21:29 -0500

    memoized user and remaining cart funcs

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItems from '../cart-item/cart-item.component';
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItens(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItens
 })
 
 export default connect(mapStateToProps)(CartDropdown);
