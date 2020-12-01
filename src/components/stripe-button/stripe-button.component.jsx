4616e353545aaa62e2884871fb5c5be75ae5a1ec
Bob Rusch <rvrusch@gmail.com>
2020-12-01 18:48:49 -0500

    created backend payment server, linked to paynow button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceFroStripe = price * 100;
+  const priceForStripe = price * 100;
   const publishablekey = 'pk_test_51HH8NaIbn6wt6TXXWYi2p8TjPpGa3RpM1wz44hQQ1jrCmEtVriJtu7BIUmNDtWxkRxCahkLnvs7K2DXclmMR4q0Q00h1qjk27N';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successfull');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment Successful');
+      })
+      .catch(error => {
+        console.log(`Payment error: ${JSON.parse(error)}`);
+        alert(
+          'There was an issue with your payment. Please make sure the card use used was correct'
+        );
+      })
   };
 
   return (
@@ -18,7 +34,7 @@ const StripeCheckoutButton = ({ price }) => {
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
       description={`Your total is $${price}`}
-      amount={priceFroStripe}
+      amount={priceForStripe}
       panelLabel='Pay Now'
       token={onToken}
       stripeKey={publishablekey}
